08b386aeed1f7f92904050c950cf2b8bd0a5bfef
Pierre <pvbaudin@ucsc.edu>
2022-10-27 15:01:25 -0700

    remade package-lock which was needed to build, set server config to work properly

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://braingeneers.gi.ucsc.edu/shadows',
+  proxy: true, 
   app: {
     keys: env.array('APP_KEYS'),
   },
